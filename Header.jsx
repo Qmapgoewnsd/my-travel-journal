8edd5bb3988987d45a7208055ed851e66ec7f499
Qmapgoewnsd <149874357+Qmapgoewnsd@users.noreply.github.com>
2025-06-26 19:10:07 +0100

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,12 @@
+import Global from '../assets/download.png'
+export default function Header() {
+    return (
+        <>
+            <header>
+                <img src={Global} alt="globe icon" />
+                <h1>my travel journal.</h1>
+            </header>
+            <br />
+        </>
+    )
+}
